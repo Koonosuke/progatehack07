745883ea7c628e65072782c2a57499eff96d277f
tankobualpha <zitofi@mail4.uk>
2025-07-12 15:00:11 +0900

    bedrock対応

--- frontend/app/api/llm/route.ts
+++ frontend/app/api/llm/route.ts
@@ -1,13 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request){
-	const OLLAMA_API_URL = process.env.OLLAMA_BASE_URL || '';
-
-	if(!OLLAMA_API_URL){
-		console.error('OLLAMA_API_URL is not configured.');
-		return NextResponse.json({ error: 'OLLAMA_API_URLがサーバーに設定されていません。' }, { status:500 });
-	}
-
+	const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || '';
 	try{
 		const { words } = await request.json();
 
@@ -18,24 +12,22 @@ export async function POST(request: Request){
 		const prompt = `あなたは今一対一通話のサポーターです．対象者はろう者です．対象者がした手話から以下の単語リストを生成しました．\n\n単語リスト: ${words.join(',')}\n\n手話の特性を考慮しつつ，単語リストを参考に文脈を補ってなめらかな文章を生成してください。ただし推測した文章以外は出力しないでください．\n例: "このユーザは「飲み会にいこう」と言っています\n →飲み会に行こう "\n\n<Ollama>`
 
 		//APIリクエスト送信
-		const response = await fetch(OLLAMA_API_URL, {
+		const response = await fetch(`${API_BASE_URL}/chat`, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 			},
-			body : JSON.stringify({
-				model: process.env.OLLAMA_MODEL,
-				prompt: prompt,
-				stream: false,
-			}),
+			body : JSON.stringify({prompt}),
 		});
+
 		if(!response.ok){
 			const errorText = await response.text();
-			throw new Error(`Ollama APIからのエラー: ${response.statusText}-${errorText}`);
+			throw new Error(`APIからのエラー: ${response.statusText}-${errorText}`);
 		}
 
-		const ollamaData = await response.json();
-		return NextResponse.json({ generatedText: ollamaData.response});
+		const data = await response.json();
+		console.log(data);
+		return NextResponse.json({ generatedText: data.generated_text});
 	} catch (error){
 		console.error('LLM API Error: ', error);
 		return NextResponse.json({ error: 'サーバー内部でエラーが発生しました。'}, { status: 500});
